35e1f1b8d10ce159615614291c402c5e42204f29
EJ Fox <ejfox@ejfox.com>
2023-08-03 12:07:40 -0400

    build: set `ssr` to false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false, // for netlify deploy
   devtools: { enabled: true },
   css: [
     'tachyons/css/tachyons.css',
