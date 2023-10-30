12f6208d5e6fd8aa5530607e7cd262a8ad6fd7b1
EJ Fox <ejfox@ejfox.com>
2023-10-29 22:53:45 -0400

    refactor(contrastTextColor): remove unused code from

--- helpers.js
+++ helpers.js
@@ -6,9 +6,8 @@ function yiq(color) {
 }
 
 function contrastTextColor(backgroundColor) {
-  if (!backgroundColor) return "#000000";
-  // turn the bg color into a d3 color so we can do color math
-  const d3Color = d3.color(backgroundColor);
+  if (!backgroundColor) return "#000000";  
+  // const d3Color = d3.color(backgroundColor);
   // const colorHsl = d3.hsl(d3Color);
 
   // figure out if the bg is light or dark based on our threshold
@@ -16,9 +15,9 @@ function contrastTextColor(backgroundColor) {
 
   // use d3 hsl to get the lightness
   // const lightness = colorHsl.l;
+  
+  // or use our yiq function to get the lightness
   const lightness = yiq(backgroundColor);
-
-  // console.log('lightness', lightness, threshold)
 
   // if the lightness is above our threshold, return an extremely darkened version of the color
   if (lightness > threshold) {
