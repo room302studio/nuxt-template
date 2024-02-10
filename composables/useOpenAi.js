1d2bba9fa2913751f345841bd7b808567b544126
EJ Fox <ejfox@ejfox.com>
2024-02-10 18:49:47 -0500

    refactor: update openai to use latest API updates

--- composables/useOpenAi.js
+++ composables/useOpenAi.js
@@ -1,95 +1,113 @@
-import { Configuration, OpenAIApi } from 'openai';
-import { OpenAIExt } from 'openai-ext';
-import { encode } from '@nem035/gpt-3-encoder';
-
-// Map of model names to their respective token costs
-const TOKEN_COSTS = {
-  'gpt-3.5-turbo': 0.002,
-  'gpt-4': 0.03
-};
-
-// Function to count tokens in given array of messages
-function countMessageTokens(messageArray = []) {
-  let totalTokens = 0;
-  messageArray.forEach((message) => {
-    const encodedMessage = encode(JSON.stringify(message)); // Replace with your custom encoding function if needed.
-    totalTokens += encodedMessage.length;
-  });
-  return totalTokens;
-}
-
-// Function to calculate and display cost of messages
-function messagesToCost(messageArray = [], model, $toast) {
-  const messageTokens = countMessageTokens(messageArray);
-  const tokenCost = TOKEN_COSTS[model];
-  // Round the total cost to 3 decimal places
-  const totalCost = (messageTokens / 1000 * tokenCost).toFixed(3);
-
-  const costText = `Cost for ${model}: $${totalCost} - Tokens: ${messageTokens}`;
-  $toast.info(costText);
-}
+import { Configuration, OpenAIApi } from 'openai'
+import { OpenAIExt } from 'openai-ext'
+import { encode } from '@nem035/gpt-3-encoder'
 
 export default function useOpenAi(apiKey) {
   // Create the OpenAI client
   const openai = new OpenAIApi(
     new Configuration({
-      apiKey,
-    }),
-  );
+      apiKey
+    })
+  )
 
-  // Function to get chat completion stream
-  async function getChatCompletionStream(promptMessages = [], options = {}, onContent, onDone, onError) {
-    const { $toast } = useNuxtApp();
-    const model = options.model;
-    
-    messagesToCost(promptMessages, model, $toast);
-
-    const streamConfig = {
-      openai,
-      apiKey,
-      handler: {
-        onContent,
-        onDone,
-        onError,
-      },
-    };
+  // Function to get chat completion
+  async function getChatCompletion(
+    promptMessages = [],
+    options = {
+      model: 'gpt-4-turbo-preview'
+    }
+  ) {
+    const cost = messagesToCost(promptMessages, options.model)
+    console.log(`Cost: $${cost}`)
 
     try {
-      OpenAIExt.streamClientChatCompletion(
-        {
-          ...options,
-          messages: promptMessages,
-        },
-        streamConfig,
-      );
+      const completion = await openai.chat.completions.create({
+        ...options,
+        messages: promptMessages
+      })
+
+      const totalTokens = completion.usage.total_tokens
+      console.log(
+        'Cost of prompt + completion:',
+        tokensToCost(totalTokens, options.model)
+      )
+
+      return completion.data.choices[0].message
     } catch (error) {
-      console.error('Error in chat completion stream:', error);
-      throw error;
+      console.error('Error in chat completion:', error)
+      throw error
     }
   }
 
-  // Function to get chat completion
-  async function getChatCompletion(promptMessages = [], options = {}) {
-    const { $toast } = useNuxtApp();
-    const model = options.model;
-
-    messagesToCost(promptMessages, model, $toast);
-
+  async function getEmbedding(
+    text,
+    options = { model: 'text-embedding-3-small' }
+  ) {
     try {
-      const completion = await openai.createChatCompletion({
+      const embedding = await openai.embeddings.create({
         ...options,
-        messages: promptMessages,
-      });
+        input: text,
+        encoding_format: 'float'
+      })
 
-      return completion.data.choices[0].message;
+      const totalTokens = cembedding.usage.total_tokens
+      console.log(
+        'Cost of embedding:',
+        tokensToCost(totalTokens, options.model)
+      )
+
+      return embedding.data
     } catch (error) {
-      console.error('Error in chat completion:', error);
-      throw error;
+      console.error('Error in embedding:', error)
+      throw error
     }
+  }
+
+  async function getVisionCompletion(
+    prompt,
+    imageUrl,
+    options = {
+      model: 'gpt-4-vision-preview'
+    }
+  ) {
+    const completion = await openai.chat.completions.create({
+      ...options,
+      messages: [
+        {
+          role: 'user',
+          content: [
+            { type: 'text', text: prompt },
+            {
+              type: 'image_url',
+              image_url: {
+                url: imageUrl
+              }
+            }
+          ]
+        }
+      ]
+    })
   }
 
   return {
-    getChatCompletionStream,
     getChatCompletion,
-  };
+    getEmbedding,
+    getVisionCompletion
+  }
+}
+
+// Map of model names to their respective token costs
+const TOKEN_COSTS = {
+  'gpt-3.5-turbo': 0.002,
+  'gpt-4': 0.03,
+  'ada-embedding': 0.0001,
+  'text-embedding-3-small': 0.00002
+}
+
+// Function to calculate and display cost of messages
+function tokensToCost(tokenCount, model) {
+  const tokenCost = TOKEN_COSTS[model]
+  // Round the total cost to 3 decimal places
+  const totalCost = ((tokenCount / 1000) * tokenCost).toFixed(3)
+  return totalCost
 }
