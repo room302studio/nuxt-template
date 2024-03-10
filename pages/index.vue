8c84bdc1be40a5b58882a9210520d30f956149af
EJ Fox <ejfox@ejfox.com>
2024-03-09 19:02:27 -0500

    feat: add splitpanes

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,15 @@
 <template>
-  <section>
-    <h1>Hello world!</h1>
+  <section class="p-4 text-4xl">
+    <Splitpanes class="min-h-screen">
+      <Pane size="50" class="p-8"> Hello </Pane>
+      <Pane size="50" class="p-8"> World </Pane>
+    </Splitpanes>
   </section>
 </template>
 
 <script setup>
+import { Splitpanes, Pane } from 'splitpanes'
+import 'splitpanes/dist/splitpanes.css'
 /*
 // Use the global state in this component
 const store = useAppStore()
@@ -13,4 +18,38 @@ const { activeItem, itemList, setActiveItem, addItem, removeItem } = store
 */
 </script>
 
-<style></style>
+<style>
+.splitpanes {}
+
+.splitpanes__splitter {
+  background-color: #ccc;
+  position: relative;
+}
+
+.splitpanes__splitter:before {
+  content: '';
+  position: absolute;
+  left: 0;
+  top: 0;
+  transition: opacity 0.4s;
+  background-color: rgba(255, 255, 255, 0.3);
+  opacity: 0;
+  z-index: 1;
+}
+
+.splitpanes__splitter:hover:before {
+  opacity: 1;
+}
+
+.splitpanes--vertical>.splitpanes__splitter:before {
+  left: -12px;
+  right: -12px;
+  height: 100%;
+}
+
+.splitpanes--horizontal>.splitpanes__splitter:before {
+  top: -12px;
+  bottom: -12px;
+  width: 100%;
+}
+</style>
